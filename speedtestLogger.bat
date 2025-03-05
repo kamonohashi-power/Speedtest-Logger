19ff8046f25db483c659c69e085f285a973a6f1e
パワーカモノハシ <ninntenndou3ds+platypus@gmail.com>
2025-03-05 18:18:28 +0900

    Update speedtestLogger.bat
    @change
    Aggregated into SERVER_ID variable

--- speedtestLogger.bat
+++ speedtestLogger.bat
@@ -4,13 +4,14 @@ set SPEEDTEST=%~dp0speedtest.exe
 set LOG_DIR=%~dp0Log
 set LOG_FILE=%LOG_DIR%\log_speedtest.csv
 set TEMP_FILE=%TEMP%\speedtest_tmp.txt
+set SERVER_ID=48463
 
 if not exist "%LOG_DIR%" mkdir "%LOG_DIR%"
 
 set NOW_TIME="%date% %time%" 
 
 if not exist "%LOG_FILE%" (
-    %SPEEDTEST% -f csv -s 48463 --output-header > "%TEMP_FILE%"
+    %SPEEDTEST% -f csv -s %SERVER_ID% --output-header > "%TEMP_FILE%"
     
     set HEADER_WRITTEN=0
     for /f "usebackq tokens=* delims=" %%A in ("%TEMP_FILE%") do (
@@ -23,7 +24,7 @@ if not exist "%LOG_FILE%" (
         )
     )
 ) else (
-    for /f "usebackq tokens=* delims=" %%A in (`%SPEEDTEST% -f csv -s 48463`) do (
+    for /f "usebackq tokens=* delims=" %%A in (`%SPEEDTEST% -f csv -s %SERVER_ID%`) do (
         echo %NOW_TIME%,%%A >> "%LOG_FILE%"
     )
 )
